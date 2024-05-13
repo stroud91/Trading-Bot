b3b668cd0472132e5e0b16eacebf1fffa53ecf3a
Ledian Fekaj <stroud91official@gmail.com>
2024-05-13 16:57:15 -0400

    changes made to alembic version

--- app/config.py
+++ app/config.py
@@ -3,10 +3,15 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     ALPHA_VANTAGE_API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url:
+        SQLALCHEMY_DATABASE_URI = database_url.replace('postgres://', 'postgresql://')
+    else:
+        raise ValueError("No DATABASE_URL set for Flask application")
+
     SQLALCHEMY_ECHO = True
