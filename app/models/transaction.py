cbbe338cc296681b658e95f7a0043153e9c27091
Ledian Fekaj <stroud91official@gmail.com>
2024-05-13 18:24:48 -0400

    changes made to alembic version with db url 3

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -14,6 +14,7 @@ class Transaction(db.Model):
     # Relationship
     account = relationship('Account', back_populates='transactions')
 
+
     def to_dict(self):
         return {
             'id': self.id,
