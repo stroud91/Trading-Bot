9b567a9b17e879dd2407e188f4bc147a7ac91a6e
Ledian Fekaj <stroud91official@gmail.com>
2024-06-05 14:30:37 -0400

    major updates css part 2

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -4,6 +4,7 @@ import { logout } from "../../store/session";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import './Navigation.css'; // Ensure it is importing the Navigation.css file
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -11,43 +12,39 @@ function ProfileButton({ user }) {
   const ulRef = useRef();
 
   const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
+    setShowMenu(!showMenu);
   };
 
   useEffect(() => {
-    if (!showMenu) return;
-
     const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
         setShowMenu(false);
       }
     };
 
     document.addEventListener("click", closeMenu);
-
     return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+  }, []);
 
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logout());
   };
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+  const ulClassName = `profile-dropdown ${showMenu ? 'show' : ''}`;
   const closeMenu = () => setShowMenu(false);
 
   return (
-    <>
-      <button onClick={openMenu}>
+    <div className="profile-button-container">
+      <button className="profile-button" onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li>
+            <li className="dropdown-item">{user.username}</li>
+            <li className="dropdown-item">{user.email}</li>
+            <li className="dropdown-item">
               <button onClick={handleLogout}>Log Out</button>
             </li>
           </>
@@ -58,7 +55,6 @@ function ProfileButton({ user }) {
               onItemClick={closeMenu}
               modalComponent={<LoginFormModal />}
             />
-
             <OpenModalButton
               buttonText="Sign Up"
               onItemClick={closeMenu}
@@ -67,7 +63,7 @@ function ProfileButton({ user }) {
           </>
         )}
       </ul>
-    </>
+    </div>
   );
 }
 
